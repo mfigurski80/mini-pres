cc9dfee48aab953524cb1ffc426b4085b51c9bc8
Mikolaj Figurski <meek.f80@gmail.com>
2018-11-19 12:45:44 -0500

    Extracted left column into fixed 'nav'

--- src/Slide.js
+++ src/Slide.js
@@ -8,31 +8,12 @@ class Slide extends React.Component {
       <div className={"slide container " + (this.props.isCurrent ? "" : "hidden")}>
         <div className="slide-grid-layout">
           <div className="slide-grid-col1">
-
-            <div className="slide-pos">
-              <p>0{this.props.nth}</p>
-              <hr />
-              <p>0{this.props.outOf}</p>
-            </div>
-
-            <h1>{this.props.info.title}</h1>
-
-            <p>{this.props.info.sub}</p>
-
-            <div className="slider-control">
-              <div className="button-control" onClick={this.props.controls.left}>
-                <i className="fas fa-arrow-left" />
-              </div>
-              <div className="button-control" onClick={this.props.controls.right}>
-                <i className="fas fa-arrow-right" />
-              </div>
-            </div>
           </div>
           <div className="slide-grid-col2">
 
           </div>
           <div className="slide-grid-col3">
-
+            <p>HELLOOO</p>
           </div>
         </div>
       </div>
--- src/Slider.js
+++ src/Slider.js
@@ -7,9 +7,8 @@ class Slider extends React.Component {
     super(props);
     this.state = {
       slidesInfo: [
-        {title: "Embrace the White Space", sub: "White space provides ease for eyes. You should use it more."},
-        {title: "Never Enough"},
-        {title: "Designs frequently lack Nothing"},
+        {title: "What is it?"},
+        {title: "What's it for?"}
       ],
       curSlide: 0,
     }
@@ -44,20 +43,40 @@ class Slider extends React.Component {
       <Slide
         key={slide.title}
         info={slide}
-        controls={{"left": this.slideLeft, "right": this.slideRight}}
-        nth={pos+1}
-        outOf={slidesInfo.length}
-        isCurrent={pos == this.state.curSlide}
+        isCurrent={pos === this.state.curSlide}
       />
     );
 
     return(
-      <div className="slider-sheet" style={{
-        width: slidesInfo.length + "00%",
-        gridTemplateColumns: "repeat(" + slidesInfo.length + ", 1fr)",
-        transform: "translateX(" + -100*this.state.curSlide/slidesInfo.length + "%)",
-      }}>
-        {slidesElem}
+      <div className="slot-plugin">
+        <div className="slider-controls">
+          <div className="slide-pos">
+            <p>0{this.state.curSlide + 1}</p>
+            <hr />
+            <p>0{slidesInfo.length}</p>
+          </div>
+
+          <h1>Embrace the White Space</h1>
+
+          <p>Take your UI to the next level</p>
+
+          <div className="slider-control">
+            <div className="button-control" onClick={this.slideLeft}>
+              <i className="fas fa-arrow-left" />
+            </div>
+            <div className="button-control" onClick={this.slideRight}>
+              <i className="fas fa-arrow-right" />
+            </div>
+          </div>
+        </div>
+        <div className="slider-sheet" style={{
+          width: slidesInfo.length + "00%",
+          gridTemplateColumns: "repeat(" + slidesInfo.length + ", 1fr)",
+          transform: "translateX(" + -100*this.state.curSlide/slidesInfo.length + "%)",
+        }}>
+
+          {slidesElem}
+        </div>
       </div>
     );
   }
